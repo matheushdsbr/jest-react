2f27e81145bb065d662a85c35295617f5ee43780
Matheus Silva <matheushds.br@gmail.com>
2021-07-27 19:03:45 -0300

    feat: add selected payment to dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import { useState } from 'react';
+import Dropdown from "./components/Dropdown";
+
 function App() {
+  const [selectedPayment, setSelectedPayment] = useState(null);
+
   return (
-    <div>Dropdown</div>
+    <>
+      {selectedPayment && (
+        <div>
+          <p>Pagamento selecionado: {selectedPayment}</p>
+        </div>
+      )}
+
+      <Dropdown
+        title="Selecione a sua opção de pagamento"
+        options={['Crédito', 'Débito', 'Dinheiro']}
+        onSelect={setSelectedPayment}
+      />
+    </>
   );
 }
 
