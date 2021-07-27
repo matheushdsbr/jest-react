ffc4af4d4cc6ba2cc64df1ad4978a70c70016b70
Matheus Silva <matheushds.br@gmail.com>
2021-07-27 19:32:47 -0300

    test(Dropdown): should signal an option was selected and close the dropdown

--- src/components/Dropdown/Dropdown.test.js
+++ src/components/Dropdown/Dropdown.test.js
@@ -27,4 +27,24 @@ describe('Dropdown', () => {
     expect(screen.getByRole('menuitem',{ name: options[1] })).toBeInTheDocument();
     expect(screen.getByRole('menuitem',{ name: options[2] })).toBeInTheDocument();
   });
+
+  it('should signal an option was selected and close the dropdown', () => {
+    const onSelect = jest.fn();
+
+    render(<Dropdown title={title} options={options} onSelect={onSelect} />);
+
+    userEvent.click(screen.getByRole('button', { name: title }));
+
+    expect(screen.getByRole('menuitem', { name: options[0] })).toBeInTheDocument();
+    expect(screen.getByRole('menuitem', { name: options[1] })).toBeInTheDocument();
+    expect(screen.getByRole('menuitem', { name: options[2] })).toBeInTheDocument();
+
+    userEvent.click(screen.getByRole('menuitem', { name: options[1] }));
+
+    expect(onSelect).toHaveBeenCalledWith(options[1]);
+
+    expect(screen.queryByText(options[0])).not.toBeInTheDocument();
+    expect(screen.queryByText(options[1])).not.toBeInTheDocument();
+    expect(screen.queryByText(options[2])).not.toBeInTheDocument();
+  });
 });
