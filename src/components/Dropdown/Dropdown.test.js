79484cf9cb1ee05fd2610f8f41d8b5d760cd0d0f
Matheus Silva <matheushds.br@gmail.com>
2021-07-27 19:16:01 -0300

    test(Dropdown): should show options when open

--- src/components/Dropdown/Dropdown.test.js
+++ src/components/Dropdown/Dropdown.test.js
@@ -1,6 +1,6 @@
-import { Dropdown } from './Dropdown';
+import Dropdown from './index';
 
-import { screen, render } from '../tests';
+import { screen, render, userEvent } from '../tests';
 
 const title = 'Selecione a sua opção de pagamento';
 const options = ['Crédito', 'Débito', 'Dinheiro'];
@@ -12,5 +12,19 @@ describe('Dropdown', () => {
     expect(screen.queryByText(options[0])).not.toBeInTheDocument();
     expect(screen.queryByText(options[1])).not.toBeInTheDocument();
     expect(screen.queryByText(options[2])).not.toBeInTheDocument();
+  });
+
+  it('should show options when open', () => {
+    render(<Dropdown title={title} options={options} onSelect={() => {}} />);
+
+    expect(screen.queryByText(options[0])).not.toBeInTheDocument();
+    expect(screen.queryByText(options[1])).not.toBeInTheDocument();
+    expect(screen.queryByText(options[2])).not.toBeInTheDocument();
+
+    userEvent.click(screen.getByRole('button', { name: title }));
+
+    expect(screen.getByRole('menuitem',{ name: options[0] })).toBeInTheDocument();
+    expect(screen.getByRole('menuitem',{ name: options[1] })).toBeInTheDocument();
+    expect(screen.getByRole('menuitem',{ name: options[2] })).toBeInTheDocument();
   });
 });
